88d45e090ed26f216c9842593e357246572bd0a3
thanhtung <tung.nguyen@se-ec.com.vn>
2020-06-15 14:06:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+# Setup and Run
+
+### Required
+```
+node v10.16.0
+npm v6.10.0
+```
+
+### Install
+```
+npm install
+```
+
+  ### Run
+```
+npm start
+```
---
+++ front/templates/js/default.js
@@ -0,0 +1,82 @@
+// BEGIN: WOW.JS
+$(document).ready(function () {
+    $('.slider-banner').slick({
+      dots: false,
+      infinite: true,
+      speed: 500,
+      fade: false,
+      arrows: false,
+      cssEase: 'linear'
+    });
+    $('.slider-work').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+    $('.slider-good-work').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+  });
+  // END: WOW.JS
